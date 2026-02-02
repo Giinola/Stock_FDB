af533fc59a3e3feeecdd2d71dd419e75cef8959f
Francky Marcel <rafalifrancky03@gmail.com>
2026-02-02 12:43:51 +0400

    affichage des produits

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.gestion_stock {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens pkg.gestion_stock to javafx.fxml;
+    opens pkg.gestion_stock.controllers to javafx.fxml;
+    opens pkg.gestion_stock.models to javafx.base;  // ← Changé de javafx.fxml à javafx.base
+
+    exports pkg.gestion_stock;
+}
